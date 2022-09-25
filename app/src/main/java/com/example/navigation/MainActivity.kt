8d356410fc96e5c42e739827c72dd7089a781568
Dennis Okumu <dennisokumu72@yahoo.com>
2022-09-25 17:13:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,163 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.navigation.ui.theme.NavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationTheme {
+                // A surface container using the 'background' color from the theme
+               val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(
+                            items = listOf(
+                                BottomNavItem(
+                                    name = "Home",
+                                    route = "Home",
+                                    icon = Icons.Default.Home
+                                ),
+                                BottomNavItem(
+                                    name = "Status",
+                                    route = "Status",
+                                    icon = Icons.Default.Notifications,
+                                    badgeCount = 102
+                                ),
+                                BottomNavItem(
+                                    name = "Account",
+                                    route = "Account",
+                                    icon = Icons.Default.Settings,
+                                    badgeCount = 2
+                                )
+                            ),
+                            navController = navController
+                        ) {
+                            navController.navigate(it.route)
+                        }
+                    }
+                ) {
+                    Navigation(navController = navController)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Navigation(navController: NavController) {
+    NavHost(navController = navController as NavHostController, startDestination = "Home" ) {
+        composable("Home"){
+            HomeScreen()
+        }
+        composable("Status"){
+            StatusScreen()
+        }
+        composable("Account"){
+            AccountScreen()
+        }
+
+    }
+    
+}
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Home Screen")
+    }
+}
+
+@Composable
+fun StatusScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Status Screen")
+    }
+}
+
+@Composable
+fun AccountScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Account Screen")
+    }
+}
+
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    onItemClick:(BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = Modifier,
+        backgroundColor = Color.DarkGray,
+        elevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item)},
+                selectedContentColor = Color.Yellow,
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    Column(
+                        horizontalAlignment = CenterHorizontally
+                    ) {
+                        if (item.badgeCount > 0){
+                            BadgedBox(badge = { Badge { Text(text = item.badgeCount.toString()) } }) {
+                                Icon(imageVector = item.icon, contentDescription = item.name)
+                            }
+
+                        } else {
+                            Icon(imageVector = item.icon, contentDescription = item.name)
+                        }
+                        if (selected) {
+                            Text(
+                                text = item.name,
+                                textAlign = TextAlign.Center,
+                                fontSize = 15.sp
+                                )
+                        }
+                    }
+                }
+            )
+        }
+
+    }
+}
